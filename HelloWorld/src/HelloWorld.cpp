ee482c341d0b7be1209778a08e33e139f9c692a8
dan-bruce <bruce.dan@outlook.com>
2012-12-12 23:34:06 +0800

    this is my first time

---
+++ HelloWorld/src/HelloWorld.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : HelloWorld.cpp
+// Author      : LD
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "!!!Hello World!!!" << endlsfggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggg; // prints !!!Hello World!!!sdfaaaaaaaaffffffffffffffffffffffffffffff3456222222222222
+    return 0;
+}
